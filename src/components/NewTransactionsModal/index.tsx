3de01ea20b8810218f5c5207d09d17b63d008cc8
Bruno Azevedo <bruno.ds.azevedo@gmail.com>
2022-02-21 07:57:44 -0300

    criado estados para controlar inputs da minha aplicacao

--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, FormEvent } from 'react';
 import Modal from 'react-modal';
 
 import closeImg from '../../assets/close.svg';
@@ -13,7 +13,14 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+	const [ title, setTitle ] = useState('');
+	const [ value, setValue ] = useState(0);
+	const [ category, setCategory ] = useState('');
 	const [ type, setType ] = useState('deposit');
+
+	function handleCreateNewTransaction(e: FormEvent) {
+		e.preventDefault();
+	}
 
 	return (
 		<Modal
@@ -29,16 +36,20 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
 			>
 				<img src={closeImg} alt="Fechar modal" />
 			</button>
-			<Container>
+			<Container onSubmit={handleCreateNewTransaction}>
 				<h2>Cadastrar transação</h2>
 				
 				<input
 					placeholder="Título"
+					value={title}
+					onChange={e => setTitle(e.target.value)}
 				/>
 
 				<input
 					type="number"
 					placeholder="Valor"
+					value={value}
+					onChange={e => setValue(Number(e.target.value))}
 				/>
 
 				<TransactionTypeContainer>
@@ -65,6 +76,8 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
 
 				<input
 					placeholder="Categoria"
+					value={category}
+					onChange={e => setCategory(e.target.value)}
 				/>
 
 				<button
